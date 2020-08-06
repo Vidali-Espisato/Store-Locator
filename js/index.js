a9be0ec3ec884670f5c2c5b65f2a13d2f0d03b08
Vidali-Espisato <manish.11612939@lpu.in>
2020-08-06 18:31:49 +0530

    Dynamic stores list generation

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
 function initMap() {
-  var jalandhar = { lat: 31.2569, lng: 75.4432 };
+  var losAngeles = { lat: 34.06338, lng: -118.35808 };
   map = new google.maps.Map(document.getElementById("map"), {
-    center: jalandhar,
+    center: losAngeles,
     zoom: 11,
     mapTypeId: "roadmap",
   });
 }
+function displayStores() {
+  var storeHTML = "";
+  var count = 0;
+  stores.forEach(function (store) {
+    count++;
+    var address = store["addressLines"];
+    var phone = store["phoneNumber"];
+    storeHTML += `
+        <div class="store-container">
+          <div class="store-number-container">
+            <div class="store-number">
+              ${count}
+            </div>
+          </div>
+          <div class="store-info-container">
+            <div class="store-address">
+              <span>${address[0]}</span>
+              <span>${address[1]}</span>
+            </div>
+            <div class="store-contact">${phone}</div>
+          </div>
+        </div>
+      `;
+    document.querySelector(".stores-list").innerHTML = storeHTML;
+  });
+}
+
+window.onload = function () {
+  displayStores();
+};
